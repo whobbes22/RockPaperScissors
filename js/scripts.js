fdb4f760fd0cd2afcbbe02f98040a0017a8e38fe
Joe W <whobbes22@gmail.com>
2023-03-30 23:05:03 -0700

    add buttons to choose if you want Rock Paper or Scissors

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,39 @@
+// non UI stuff
+
+
+
+
+// UI Logic
 window.addEventListener("load", function(){
-  const form = document.querySelector("#rock");
-  form.addEventListener("submit", playGame);
+  const rock1 = document.querySelector("#rock");
+  rock1.addEventListener("click", chooseRock);
+  const paper1 = document.querySelector("#paper");
+  paper1.addEventListener("click", choosePaper);
+  const scissor1 = document.querySelector("#scissor");
+  scissor1.addEventListener("click", chooseScissor);
   // const form2 = document.querySelector("#sub2");
   // form2.addEventListener("submit", choseHero);
-})
+});
 
-function playGame(event){
+function chooseRock (event){
   event.preventDefault();
-  const chosenHero = document.querySelector("#rock").value;
-  const chosenHero2 = document.querySelector("#paper").value;
-  console.log(chosenHero,chosenHero2);
+  const rock = document.querySelector("#rock").value;
+  playGame(rock);
+}
+
+function choosePaper (event){
+  event.preventDefault();
+  const paper = document.querySelector("#paper").value;
+  playGame(paper);
+}
+function chooseScissor (event){
+  event.preventDefault();
+  const scissor = document.querySelector("#scissor").value;
+  playGame(scissor);
+}
+      
+function playGame(hero){
+  console.log("hi my hero is " + hero);
 }
 
 
