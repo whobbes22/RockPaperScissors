3d87ecbdd631140d1409d5458b567086b838016e
Joe W <whobbes22@gmail.com>
2023-03-30 22:44:29 -0700

    added random score for oppoenent to play

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 window.addEventListener("load", function(){
-  
+  const form = document.querySelector("#rock");
+  form.addEventListener("submit", playGame);
+  // const form2 = document.querySelector("#sub2");
+  // form2.addEventListener("submit", choseHero);
 })
+
+function playGame(event){
+  event.preventDefault();
+  const chosenHero = document.querySelector("#rock").value;
+  const chosenHero2 = document.querySelector("#paper").value;
+  console.log(chosenHero,chosenHero2);
+}
+
+
+function opponentPlays(){
+  // random number between 0 and 100
+  const score = Math.round(Math.random()*100);  
+
+  if(score < 33){
+    return "rock";
+  } else if( score < 66){
+    return "paper";
+  } else {
+    return "scissor"
+  }
+}
